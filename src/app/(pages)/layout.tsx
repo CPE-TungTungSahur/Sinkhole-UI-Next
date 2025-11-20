031a7b2da79f9524d29415a14d239499d0169a6f
ImJustNon <Im_Just_Non@hotmail.com>
2025-11-20 10:22:26 +0700

    Add : Navbar

--- src/app/(pages)/layout.tsx
+++ src/app/(pages)/layout.tsx
@@ -1,9 +1,11 @@
+import Navbar from "@/components/Navbar";
 import React from "react";
 
 export default function PageLayout({ children }: { children: React.ReactNode }): React.JSX.Element {
     return (
         <>
-            <h1></h1>
+            <Navbar />
+            {children}
         </>
     );
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,34 @@
-import React from "react";
+"use client";
+
+import { usePathname } from "next/navigation";
+import React, { useEffect, useState } from "react";
 
 export default function Navbar(): React.JSX.Element {
+    const pathname = usePathname();
+    const [des, setDes] = useState<{
+        name: string;
+        path: string;
+    }>();
+
+    useEffect(() => {
+        console.log();
+        setDes({
+            name: pathname === "/" ? "Map" : "Home",
+            path: pathname === "/" ? "/map" : "/",
+        });
+    }, [pathname]);
     return (
         <>
-            <h1>Navbar</h1>
+            <div className="border-border fixed left-0 top-0 z-50 w-full border-b bg-[#0f1629]/80 backdrop-blur-lg">
+                <div className="container mx-auto w-full">
+                    <div className="flex flex-row items-center justify-between px-10 py-4">
+                        <div className="text-2xl font-bold text-white">Sinkhole</div>
+                        <div className="w-20 cursor-pointer rounded-lg border-[1px] border-[#12cef0] bg-transparent px-3 py-2 text-center text-white shadow-lg duration-150 hover:bg-[#12cef0]">
+                            {des?.name}
+                        </div>
+                    </div>
+                </div>
+            </div>
         </>
     );
 }
