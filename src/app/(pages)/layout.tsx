81edcfb9203aa23d74a09cacd53d60123d822a75
ImJustNon <Im_Just_Non@hotmail.com>
2025-12-09 04:11:06 +0700

    Add : Loadingbar

--- src/app/(pages)/layout.tsx
+++ src/app/(pages)/layout.tsx
@@ -1,13 +1,16 @@
+import LoadingBar from "@/components/LoadingBar";
 import Navbar from "@/components/Navbar";
+import { LoadingProvider } from "@/contexts/LoadingContext";
 import React from "react";
 
 export default function PageLayout({ children }: { children: React.ReactNode }): React.JSX.Element {
     return (
-        <>
+        <LoadingProvider>
             <div className="">
+                <LoadingBar />
                 <Navbar />
                 {children}
             </div>
-        </>
+        </LoadingProvider>
     );
 }
