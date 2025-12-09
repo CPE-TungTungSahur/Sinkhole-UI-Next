80883e181437ed072825d0f118e66e6dfa75a163
ImPatipol <122760030+BooMzXI@users.noreply.github.com>
2025-12-09 14:54:59 +0700

    Create get-feature api and get data

--- src/components/PointDetailsDrawer.tsx
+++ src/components/PointDetailsDrawer.tsx
@@ -1,8 +1,16 @@
 import { Drawer } from "antd";
 import { X } from "lucide-react";
 import React from "react";
+import { IGeoJSONFeature, PointFeatureResponse } from "@/app/(pages)/map/page";
+interface PointDetailsDrawerProps {
+    isOpen: boolean;
+    onClose: () => void;
+    feature: IGeoJSONFeature | null;
+    pointFeatureData: PointFeatureResponse | null;
+    isFeatureLoading: boolean;
+}
 
-export default function PointDetailsDrawer({ isOpen, onClose }: { isOpen: boolean; onClose: () => void }): React.JSX.Element {
+export default function PointDetailsDrawer({ isOpen, onClose, feature, pointFeatureData, isFeatureLoading }: PointDetailsDrawerProps): React.JSX.Element {
     return (
         <Drawer
             className="rounded-2xl"
@@ -22,6 +30,18 @@ export default function PointDetailsDrawer({ isOpen, onClose }: { isOpen: boolea
                 <div className="ml-5 text-2xl font-bold text-cyan-400">
                     Prediction <span className="text-white">Details</span>
                 </div>
+                {/*  เขียนตรงนี้จ้า  */}
+                {isFeatureLoading ? (
+                    <div className="ml-auto mr-4 text-white">Loading...</div> // ใส่หน้าโหลด
+                ) : pointFeatureData && feature ? ( // ตรวจสอบว่ามีข้อมูลไหม
+                    <div className="ml-auto mr-4 text-white">
+                        <div>Latitude: {pointFeatureData.point.lat.toFixed(10)}</div>
+                        <div>Longitude: {pointFeatureData.point.lon.toFixed(10)}</div>
+                    </div>
+                ) : (
+                    <div className="ml-auto mr-4 text-white">No data available</div>
+                )}
+                {/*  เขียนตรงนี้จ้า  */}
             </div>
         </Drawer>
     );
