62d80daa6d4a88ea6ae71436eaeb298c9a821607
ImJustNon <Im_Just_Non@hotmail.com>
2025-12-14 00:25:24 +0700

    Close context menu after req

--- src/app/(pages)/map/page.tsx
+++ src/app/(pages)/map/page.tsx
@@ -346,6 +346,7 @@ export default function MapPage() {
             }
         } finally {
             stopLoading();
+            setIsOpenInterestCoordinatesContextMenu(false);
         }
     }
 
